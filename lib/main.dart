fc2676e0656ede71fc466fc7118a12168e8be22e
Vasavika Ravilla <vravilla@nexillo.com>
2026-07-02 02:15:31 +0530

    Import ThemeMode and themeModeProvider to fix compilation failure

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'presentation/providers/user_provider.dart';
 import 'utils/theme.dart';
 import 'presentation/screens/splash_screen.dart';
 
--- lib/presentation/providers/user_provider.dart
+++ lib/presentation/providers/user_provider.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:uuid/uuid.dart';
 import '../../data/local/secure_storage_service.dart';
