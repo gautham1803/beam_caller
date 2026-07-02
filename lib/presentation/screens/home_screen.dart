98c9806cffcdd6edb421c921be886d93585648c0
Vasavika Ravilla <vravilla@nexillo.com>
2026-07-02 17:29:40 +0530

    Fix missing services.dart import in home_screen.dart for HapticFeedback

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../domain/models/call_state.dart';
 import '../../utils/theme.dart';
